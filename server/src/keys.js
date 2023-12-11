3a3ec969f62c4b6c89aef128c51bd65e4edfe7d8
Ernesto2021Me <153482630+Ernesto2021Me@users.noreply.github.com>
2023-12-11 03:26:13 -0600

    Update keys.js

--- server/src/keys.js
+++ server/src/keys.js
@@ -2,9 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
     database: {
-        host: 'localhost',
+        host: 'dbdemo.cp2yzz2qk8jx.us-east-2.rds.amazonaws.com',
         user: 'root',
-        password: '',
-        database: 'biblioteca_virtual'
+        password: 'ASDFGHJKL',
+        database: 'biblioteca_virtual2'
     }
 };
